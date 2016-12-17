f39caadc8fe96ac0797ccc822aeaa1881adbb9bc
Vedant Rathore <vedantr1998@gmail.com>
2016-12-17 23:47:31 +0530

    Added Database seed

---
+++ seed/productSeeder.js
@@ -0,0 +1,33 @@
+/**
+ * Created by nero on 17/12/16.
+ */
+
+var Product = require('../models/product');
+var faker = require('faker');
+
+var mongoose = require('mongoose');
+
+mongoose.connect('localhost:27017/shopping');
+
+var products = [];
+
+var done =0;
+
+for(var i=0;i<100;i++){
+    products.push(new Product({
+        imagePath: faker.image.imageUrl(),
+        title: faker.commerce.productName(),
+        description: faker.lorem.words(),
+        price: faker.random.number()
+    }));
+    products[i].save(function (err,result) {
+        done++;
+        if(done === products.length) {
+            exit();
+        }
+    });
+}
+
+function exit() {
+    mongoose.disconnect();
+}
