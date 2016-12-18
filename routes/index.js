1c02db84f563aac04265d6cceec796667af0435e
Vedant Rathore <vedantr1998@gmail.com>
2016-12-18 14:12:55 +0530

    Added Checkout view

--- routes/index.js
+++ routes/index.js
@@ -37,6 +37,14 @@ router.get('/shopping-cart',function (req, res, next) {
     }
     var cart = new Cart(req.session.cart);
     res.render('shop/shopping-cart',{products:cart.generateArray(),totalPrice:cart.totalPrice})
-})
+});
+
+router.get('/checkout',function (req, res, next) {
+    if(!req.session.cart){
+        return res.redirect('/shopping-cart');
+    }
+    var cart = new Cart(req.session.cart);
+    res.render('shop/checkout',{total:cart.totalPrice})
+});
 
 module.exports = router;
