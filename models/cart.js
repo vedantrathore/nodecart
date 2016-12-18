6c45cc6babd825cd2e71a47c65aedd0322340aea
Vedant Rathore <vedantr1998@gmail.com>
2016-12-18 18:38:28 +0530

    Finishing touches

--- models/cart.js
+++ models/cart.js
@@ -20,5 +20,22 @@ module.exports = function Cart(oldCart) {
             arr.push(this.items[id]);
         }
         return arr;
-    }
+    };
+
+    this.reduceByOne = function (id) {
+        this.items[id].qty--;
+        this.items[id].price -= this.items[id].item.price;
+        this.totalQty--;
+        this.totalPrice -= this.items[id].item.price;
+
+        if(this.item[id].qty<=0){
+            delete this.item[id];
+        }
+    };
+
+    this.remove = function (id) {
+        this.totalQty -= this.item[id].qty;
+        this.totalPrice -= this.item[id].price ;
+        delete this.item[id];
+    };
 };
